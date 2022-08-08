149cba9b2d932bf17194de3596abab5ca8696af9
vahra <vahra@DESKTOP-SO0L1VA>
2022-08-08 18:52:17 +0300

    Add project files.

---
+++ ConsoleApp16/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+
+    public class Class1
+    {
+        public static void Swap(int[] a, int i, int j)
+        {
+            int t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+
+        }
+        public static int Partition(int[] a, int l, int r)
+        {
+            int ndx = l;
+            int pivot = a[l];
+            for (int i = l + 1; i <= r; i++)
+            {
+                if (a[i] < pivot)
+                {
+                    ndx++;
+                    Swap(a, ndx, i);
+                }
+            }
+            Swap(a, ndx, l);
+            return ndx;
+        }
+        public static void QuickSort(int[] a, int l, int r)
+        {
+            if (l < r)
+            {
+                int pi = Partition(a, l, r);
+                QuickSort(a, l, pi - 1);
+                QuickSort(a, pi + 1, r);
+
+            }
+        }
+    }
+}
+
+
+
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp16;
+
+int[] a = { 3, 2, 5, 1, 7, 4 };
+Class1.QuickSort(a, 0, 5);
+for (int i = 0; i < a.Length; i++)
+{
+    Console.WriteLine(a[i]);
+}
